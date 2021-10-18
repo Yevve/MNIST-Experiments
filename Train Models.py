ae03f034059d15dbf8027c79d8593ea5fa9d2260
LucasStrand <57836852+LucasStrand@users.noreply.github.com>
2021-10-18 12:38:21 +0200

    defaulted values
    Co-Authored-By: Yevve <70255780+yevve@users.noreply.github.com>

--- Train Models.py
+++ Train Models.py
@@ -97,7 +97,7 @@ def non_convolutional_model():
     model = K.Sequential()
     model.add(K.layers.Flatten())
     model.add(K.layers.Input(28*28))
-    model.add(K.layers.Dense(1024, activation="relu"))   #32
+    model.add(K.layers.Dense(32, activation="relu"))
     model.add(K.layers.Dense(10, activation="softmax"))
     
     model.compile(loss="categorical_crossentropy",
@@ -111,7 +111,7 @@ def non_convolutional_model():
 def convolutional_model():
     model = K.Sequential()
     model.add(K.layers.Input((28,28,1)))
-    model.add(K.layers.Conv2D(16, kernel_size=(24, 24), strides=(4,4), activation="relu")) #(16,16) (2,2), 
+    model.add(K.layers.Conv2D(16, kernel_size=(8, 8), strides=(1,1), activation="relu")) #(16,16) (2,2), (24,24) (4,4)
     model.add(K.layers.MaxPooling2D())
     model.add(K.layers.Flatten())
     model.add(K.layers.Dense(10, activation="softmax"))
@@ -129,7 +129,7 @@ log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 tb_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, profile_batch=0)
 
 # Välj en modell
-model = non_convolutional_model()
+model = convolutional_model()
 
 # Träna modellen
 model.fit(x_train, y_train,
